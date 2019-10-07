2ae40d39637830b5b600f51133ccc319060a7346
Gazmir <kullagazmir@gmail.com>
2019-10-07 11:51:01 +0200

    Rehydrate activated for reducer

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -3,6 +3,3 @@ export const USER = "USER";
 export const USER_LIST_GET = "USER_LIST_GET";
 export const USER_LIST_GET_SUCCESS = "USER_LIST_GET_SUCCESS";
 export const USER_UPDATE = "USER_UPDATE";
-
-// REHYDRATE
-export const REHYDRATE = "REHYDRATE";
